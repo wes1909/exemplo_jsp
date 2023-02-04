9de67eaf01aff21d1874662fa09663450449bd3a
407787 <407787@VDA-COMP2-19155.unoesc.lan>
2023-02-04 09:33:49 -0300

    Troca de Produto

--- src/main/java/br/edu/unoesc/exemplo_jsp/IndexController.java
+++ src/main/java/br/edu/unoesc/exemplo_jsp/IndexController.java
@@ -16,7 +16,7 @@ public class IndexController {
 	@RequestMapping("/jsp")
 	String jsp(Model model) {
 		Produto p1 = new Produto("Mesa", 10, new BigDecimal("3.33"));
-		Produto p2 = new Produto("Cadeira", 20, new BigDecimal("33.33"));
+		Produto p2 = new Produto("TV", 20, new BigDecimal("33.33"));
 
 		List<Produto> produtos = new ArrayList<Produto>();
 		produtos.add(p1);
